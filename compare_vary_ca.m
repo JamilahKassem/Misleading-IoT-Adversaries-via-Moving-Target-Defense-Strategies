94c6facd23415b3282bbf61a5e36b64ab0b62342
JAMIL AHMAD KASSEM <106966435+JamilahKassem@users.noreply.github.com>
2023-10-03 14:09:28 +0200

    Update compare_vary_ca.m
    fixed mistake in calculating coef3

--- compare_vary_ca.m
+++ compare_vary_ca.m
@@ -2,19 +2,20 @@ clear;
 clc;
 close all;
 % define number of nodes and networks
-Nc=1;
-n=2;
+% Nc=1;
+% n=2;
 % Nc=13;
 % n=5;
-% Nc=4;
-% n=5;
+Nc=4;
+n=5;
 
 % migration cost
 cd=3/2;
 % attack cost
 ca=1/3;
 
-cas=0.1:0.01:1;
+%initiateset(gcf, 'PaperUnits', 'normalized') matrices for costs
+cas=0.1:0.1:1;
 Cost_old=zeros(size(cas,1));
 Cost_old_modified=zeros(size(cas,1));
 Cost_new=zeros(size(cas,1));
@@ -24,14 +25,13 @@ Utility_new=zeros(size(cas,1));
 
 C=1;%counter coefficient
 Coef1 = mod(Nc,n)*ceil(Nc/n)*(1-(floor(Nc/n)+mod(Nc,n)/n)/ceil(Nc/n))^2;
+Coef3 = 1 - Nc/(n * ceil(Nc/n));
 
 for ca=cas
+    delta = Coef3 - 1/(2*cd*mod(Nc,n)*ceil(Nc/n));
     Coef2 = (Nc-n*floor(ca))*ceil(ca)*(1-ca/ceil(ca))^2;
     t = (1 - ca/ceil(ca))^2;
     N = (Nc - n * floor(ca)) * ceil(ca);
-    math_limit_nume = 1 + sqrt(1 - N * t);
-    math_limit_denume = abs(2 * N * t); 
-    math_limit = math_limit_nume / math_limit_denume;
     
     [ Utility_old(C),Cost_old(C) ] = old_model( ca,cd,n,Nc );
     [ Utility_old_modified(C),Cost_old_modified(C) ] = old_model_modified( ca,cd,n,Nc );
@@ -39,7 +39,7 @@ for ca=cas
     Utility_old(C) = Nc*Utility_old(C);
     Cost_old_modified(C) = Nc*Cost_old_modified(C);
     Utility_old_modified(C) = Nc*Utility_old_modified(C);
-    [ Utility_new(C),Cost_new(C) ] = new_model( ca,cd,n,Nc,Coef1,Coef2,math_limit );
+    [ Utility_new(C),Cost_new(C) ] = new_model( ca,cd,n,Nc,Coef1,Coef2,delta );
     C=C+1;
 end
 h=figure;
@@ -50,7 +50,10 @@ plot(cas,Cost_new,'--','color','red');
 plot(cas,Utility_old,'-','color','blue');
 plot(cas,Utility_old_modified,'-+','color','blue');
 plot(cas,Utility_new,'--','color','blue');
+xline(0.5,'--r',{'Acceptable','Limit'});
 hold off;
 xlabel('attack cost ca');
 ylabel('Gain');
-legend('Original model cost','Modified model cost','New model cost','Original model utility','Modified model utility','New model utility');
+legend('Original model cost App1','Original model cost App2','New model cost','Original model utility App 1','Original model utility App2','New model utility');
+set(h,'papersize',[5.3 4.5]);
+print(h,['compare_ca_Nc_',num2str(Nc),'_n_',num2str(n)],'-dpdf');
